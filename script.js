5373f0a52975f905768c489c5d3f64bb6ff3bfe1
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-02 17:49:26 +0300

    выполненное задание второго урока

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
-alert("Hello world!");
-console.log("Hello console!");
+/**задание 1 */
+/*let title = "project";
+console.log(title);
+console.log(typeof title);
 
-const title = 1234;
-let screens = "1234";
-const screenPrice = "price";
-const rollback = 456;
-let fullPrice = "one";
-const adaptive = 1909;
+let screens = "Простые, Сложные, Интерактивные";
+console.log(screens);
+
+const screenPrice = 145;
+console.log(screenPrice);
+
+const rollback = 26;
+console.log(rollback);
+
+const fullPrice = 1234567876543;
+console.log(fullPrice);
+
+const adaptive = true;
+console.log(adaptive);*/
+
+
+/*задание 2 */
+//1
+  /*let title = "project";
+  console.log(typeof title);
+
+  const fullPrice = 1234567876543;
+  console.log(typeof fullPrice);
+
+  const adaptive = true;
+  console.log(typeof adaptive);*/
+
+//2
+  /*let screens = "Простые, Сложные, Интерактивные";
+  console.log(screens.length); */
+
+//3
+  /*const screenPrice = 145;
+  console.log(screenPrice);
+  console.log(screenPrice*60);
+  console.log(screenPrice*1.63);
+  console.log(screenPrice*8.95);*/
+
+//4
+  /*const fullPrice = 1234567876543;
+  console.log(fullPrice);
+  console.log(fullPrice*60);
+  console.log(fullPrice*1.63);
+  console.log(fullPrice*8.95);*/
+
+//5
+  /*let screens = "Простые, Сложные, Интерактивные";
+  console.log(screens.toLowerCase());
+  console.log(screens.split(""));
+  console.log(screens.split());*/
+
+//6
+const fullPrice = 1234567876543;
+const rollback = 26;
+console.log(fullPrice*(rollback/100));
+
+ 
+
+
 
 
