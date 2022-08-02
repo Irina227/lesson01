d44cf6cda30cc4da375cb5f9337e754399ba4a0e
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-02 11:46:17 +0300

    выполнено первое задание

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Hello world!");
+console.log("Hello console!");
+
+const title = 1234;
+let screens = "1234";
+const screenPrice = "price";
+const rollback = 456;
+let fullPrice = "one";
+const adaptive = 1909;
