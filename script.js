10db05db18fdcf6decaad14d1e0dc97ac9eeb7bc
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-04 11:33:20 +0300

    Домашнее задание урока 3

--- script.js
+++ script.js
@@ -1,11 +1,131 @@
-const n = +prompt('Введите число')
-switch (true){
-  case n > 10:
-    console.log('n > 10');
-    break
-  case n <= 10:
-    console.log('n <= 10');
-    break
-  default:
-    console.log('Не верно ни одно значение');
-}
+/**задание 1 */
+/*let title = "project";
+console.log(title);
+console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+console.log(screens);
+const screenPrice = 145;
+console.log(screenPrice);
+const rollback = 26;
+console.log(rollback);
+const fullPrice = 1234567876543;
+console.log(fullPrice);
+const adaptive = true;
+console.log(adaptive);*/
+
+
+/*задание 2 */
+//1
+  /*let title = "project";
+  console.log(typeof title);
+  const fullPrice = 1234567876543;
+  console.log(typeof fullPrice);
+  const adaptive = true;
+  console.log(typeof adaptive);*/
+
+//2
+  /*let screens = "Простые, Сложные, Интерактивные";
+  console.log(screens.length); */
+
+//3
+  /*const screenPrice = 145;
+  console.log(screenPrice);
+  console.log(screenPrice*60);
+  console.log(screenPrice*1.63);
+  console.log(screenPrice*8.95);*/
+
+//4
+  /*const fullPrice = 1234567876543;
+  console.log(fullPrice);
+  console.log(fullPrice*60);
+  console.log(fullPrice*1.63);
+  console.log(fullPrice*8.95);*/
+
+//5
+  /*let screens = "Простые, Сложные, Интерактивные";
+  console.log(screens.toLowerCase());
+  console.log(screens.split(""));
+  console.log(screens.split());*/
+
+//6
+/*const fullPrice = 1234567876543;
+const rollback = 26;
+console.log(fullPrice*(rollback/100));*/
+
+
+
+
+/*Урок 3 */
+//3
+/*let title = prompt('Как называется Ваш проект?');
+console.log(title, typeof title);*/
+
+//4
+/*let screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
+console.log(screens, typeof screens);*/
+
+//5
+/*let screenPrice = prompt('Сколько будет стоить данная работа?','50000');
+console.log(screenPrice);*/
+
+//6
+/*let adaptive  = prompt('Нужен ли адаптив на сайте?','true/false');
+console.log(adaptive);*/
+
+//7
+/*let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = prompt('Сколько это будет стоять?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = prompt('Сколько это будет стоять?');*/
+
+//8
+/*let screenPrice = prompt('Сколько будет стоить данная работа?','50000');
+let servicePrice1 = prompt('Сколько это будет стоять?');
+let servicePrice2 = prompt('Сколько это будет стоять?');
+let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+console.log(fullPrice);*/
+
+//9
+/*let screenPrice = prompt('Сколько будет стоить данная работа?','50000');
+let servicePrice1 = prompt('Сколько это будет стоять?');
+let servicePrice2 = prompt('Сколько это будет стоять?');
+let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+let air = 345.6;
+let servicePercentPrice = Number(fullPrice) - Number(air);
+console.log(fullPrice);*/
+
+//10
+
+ /*const fullPrice = +prompt('Введите число')
+  switch (true) {
+  case fullPrice > 30000: 
+      console.log('Даем скидку в 10%');
+  break
+  default: 
+      console.log('Значение не верно');
+}*/
+
+
+/*const fullPrice = +prompt('Введите число');
+switch (true) {
+    case 15000 < fullPrice && fullPrice < 30000:
+      console.log('Даем скидку в 5%');
+      break
+    default:
+      console.log('Скидка не работает')
+}*/
+
+
+/*const fullPrice = +prompt('Введите число');
+switch (true) {
+    case 0 < fullPrice && fullPrice < 15000:
+      console.log('Скидка не предусмотрена');
+      break
+}*/
+
+/*const fullPrice = +prompt('Введите число');
+switch (true) {
+    case fullPrice < 0:
+      console.log('Что-то пошло не так');
+      break
+}*/
