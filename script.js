a56535cf9b420e436e046e8e4f0a285ba0097d77
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-08 15:00:07 +0300

    урок 4

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-alert("Hello world!");
-console.log("Hello console!");
+'use strict';
 
-const title = 1234;
-let screens = "1234";
-const screenPrice = "price";
-const rollback = 456;
-let fullPrice = "one";
-const adaptive = 1909;
+// function getMessage() {
+//   alert("Hello world!");
+// }
+// getMessage();
+
+// function cleanHands(){
+//   function dirtyHands(){
+//     console.log('Испачкать руки');
+//   }
+//   function goToTheBathroom(){
+//     console.log('Пойти в ванную комнату');
+//   }
+//   function cleanHands(){
+//     console.log('Помыть руки');
+//   }
+//   dirtyHands()
+//   goToTheBathroom()
+//   cleanHands()
+// }
+// cleanHands()
+
+// let res //объявили переменную
+
+// const countSum = function(a,b) { //объявили функцию
+//     res = a + b
+// }
+// countSum(3,5) //вызов функции
+// console.log(res) //вывод в консоль
+
+// const money = 5000;
+// const expenses1 = 200;
+// const expenses2 = 300;
+
+// const countTotal = function () {
+//   return money - expenses1 - expenses2;
+// };
+// console.log(countTotal());
 
 
+/*Задание 4*/
